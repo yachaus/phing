6e18658b46992d02072dfd8ef71d9f6aea2c816c
yachaus <50131563+yachaus@users.noreply.github.com>
2019-12-17 19:04:10 +0200

    laravel rebase

--- build/production/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=127.0.0.1;dbname=test',
-    'username' => 'chaus',
-    'password' => '11111',
-    'charset' => 'utf8',
-];
--- views/site/say.php
+++
@@ -1,3 +0,0 @@
-<?php
-use yii\helpers\Html;
-echo Html::encode($message);
